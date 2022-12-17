7d428c73b86c9a5a82ad39c58e047decd5714172
Mateus <108558575+MateusToniason@users.noreply.github.com>
2022-12-16 23:30:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var n1 = Number(window.prompt('Digite um número:'));
+var n2 = Number(window.prompt('Digite outro número'));
+var soma = n1 + n2;
+document.write(`A soma desses valores é: ${soma}`);
+//Estou usando "Template string" para fazer a soma desses valores//
